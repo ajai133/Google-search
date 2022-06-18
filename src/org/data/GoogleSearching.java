ce46b5db9defda39ce914f8c1d630938216d17ae
ajai133 <ajai133@gmail.com>
2022-06-18 15:31:28 +0530

    my google search project

---
+++ src/org/data/GoogleSearching.java
@@ -0,0 +1,16 @@
+package org.data;
+
+public class GoogleSearching {
+
+	private void searchBox() {
+	
+		System.out.println("Please enter the values");
+
+	}
+	public static void main(String[] args) {
+		
+		GoogleSearching g = new GoogleSearching();
+		g.searchBox();
+		
+	}
+}
